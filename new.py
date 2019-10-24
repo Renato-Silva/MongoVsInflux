f2770b6c0a5156dd54c2d6e9d31c8ab30b597d6f
Renato Silva <renato.silva.an@gmail.com>
2019-10-24 11:25:19 +0100

    count time

--- new.py
+++ new.py
@@ -15,7 +15,7 @@ clientInflux = InfluxDBClient('localhost', 8086, 'root', 'root', 'test')
 start_time = time.time()
 print("Starting InfluxDB...")
 
-query_where = 'select * from temperature where value=30 limit 1;'
+query_where = 'select * from temperature where value=30;'
 resultInflux = clientInflux.query(query_where)
 #print("Result: {0}".format(resultInflux))
 
@@ -28,8 +28,8 @@ print("Starting MongoDB ...")
 
 
 myquery = { "value": "30" }
-#resultMongo = mycolMongo.find(myquery).limit(1)
-resultMongo = mycolMongo.find_one(myquery)
+resultMongo = mycolMongo.find(myquery)
+#resultMongo = mycolMongo.find_one(myquery)
 
 #doc_count = mydbMongo.count_documents(myquery)
 #print("Result:  " + resultMongo)
